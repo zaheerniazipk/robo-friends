2aff8f7000f7ad74574649c6759f5992387ec817
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-10-04 09:14:11 +0500

    Created CardList Component

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import Card from './components/Card';
 import 'tachyons';
+import CardList from './components/CardList';
 import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
